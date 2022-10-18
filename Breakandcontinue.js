eea4d9c4c04603ebeaa51569388999d3d42dee92
nanda nrdnsyh <nandanrdnsych@gmail.com>
2022-10-17 21:00:49 -0700

    Absen_Belajar_Functions

---
+++ Breakandcontinue.js
@@ -0,0 +1,6 @@
+for (i=0;  i<=15; i++){
+    if(i == 9){
+        break;
+    }
+    console.log(i)
+}
---
+++ functiions_usingMultipe.js
@@ -0,0 +1,7 @@
+function myfunc(x, y){
+    //somecode
+}
+function sayHello(name, age){
+    console.log(name + "is" + age + "years old");
+}
+sayHello("Steven" , 20)
