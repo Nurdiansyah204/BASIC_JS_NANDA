822ee5808c9b5e5c082b950cde0aec9a52c5ea37
nanda nrdnsyh <nandanrdnsych@gmail.com>
2022-10-30 20:44:34 -0700

    tugas_31-10-22

--- Breakandcontinue.js
+++ Breakandcontinue.js
@@ -4,3 +4,20 @@ for (i=0;  i<=15; i++){
     }
     console.log(i)
 }
+
+var i = "A"
+if (i == A) {
+    console.log("peringkat anda satu")
+} else if (i == B) {
+    console.log("anda peringkat dua")
+} else {
+    console.log("anda peringkat " + i)
+}
+
+switch (i) {
+    case "A":
+        console.log("anda peringkat 1")
+    break;
+    default :
+        console.log("anda peringkat 2")
+}
---
+++ remedial_nan.js
@@ -0,0 +1,62 @@
+// ini comment online
+/* ini comment
+ multiline */
+
+// Assignment
+var i = 10;
+i += 5 ;
+console.log(i)
+
+//camporison
+var q = 5 >= 10;
+console.log(q)
+
+//if
+
+var  b = 10;
+if(b >= 5){
+    console.log(b)
+}
+
+//if else
+var c = 10;
+if(c >= 8){
+    console.log("betul")
+}else{
+    console.log("salah")
+}
+
+//swith case
+
+var d ="a"
+switch(d){
+    case "1":
+        console.log("sangat bagus")
+        break;
+    default :
+    console.log("kurang baik")    
+}
+
+//for
+var n = 0
+for(var n = 0;n <= 5;n++){
+    console.log(n)
+}
+
+console.log("\n")
+
+//while
+var i = 5
+while(i>=0){
+    console.log(i)
+    i-=1
+}
+
+console.log("\n")
+
+//do while
+var i = 1
+do{
+    console.log(i)
+    i-=1
+}while(i>=5)
